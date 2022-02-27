bcc70108f7d4db3ba74f16b3c0d096aafb0f1f33
Aleksandr <gvardianez@yandex.ru>
2022-02-27 18:18:34 +0300

    rest_controller

--- src/main/java/ru/alov/springboot/repositories/ProductRepository.java
+++ src/main/java/ru/alov/springboot/repositories/ProductRepository.java
@@ -2,13 +2,14 @@ package ru.alov.springboot.repositories;
 
 import org.hibernate.SessionFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import ru.alov.springboot.entities.Product;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     List<Product> findProductsByCategory_Id(Long id);
 
     List<Product> findProductsByCostBetween(Double minCost, Double maxCost);
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,16 +1,17 @@
 angular.module('app', []).controller('indexController', function ($scope, $http) {
     const contextPath = 'http://localhost:8080/app';
 
-    $scope.loadProducts = function () {
+    $scope.loadProducts = function (pageIndex = 1) {
         $http({
             url: contextPath + '/products',
             method: 'GET',
             params: {
-                min_price: $scope.filter ? $scope.filter.min_price : null,
-                max_price: $scope.filter ? $scope.filter.max_price : null
+                name_part: $scope.filter ? $scope.filter.name_part : null,
+                min_cost: $scope.filter ? $scope.filter.min_cost : null,
+                max_cost: $scope.filter ? $scope.filter.max_cost : null
             }
         }).then(function (response) {
-            $scope.ProductsPage = response.data;
+            $scope.ProductsList = response.data.content;
         });
     };
 
@@ -21,10 +22,17 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
                 });
     }
 
+    $scope.createProduct = function () {
+        $http.post(contextPath + '/products', $scope.createProduct)
+            .then(function (response) {
+                $scope.loadProducts();
+            });
+    }
+
     $scope.changeScore = function (productId, delta) {
         $http({
             url: contextPath + '/products/change_cost',
-            method: 'GET',
+            method: 'Patch',
             params: {
                 productId: productId,
                 deltaCost: delta
